0860416c53efdeaa1ca6b8196a40b30e99e8b0dd
Ossi Mantylahti, TC <ossi.mantylahti@terracolligo.com>
2019-05-07 09:38:53 -0500

    Import-export app 7.5 version

---
+++ import_product_inventory/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{'name': 'Import product in Batch',
+ 'version': '11.0.7.5.2019',
+ 'category': 'other',
+ 'depends': ['l10n_mx','l10n_mx_edi','sale_stock','purchase'],
+ 'author': "Terra Colligo",
+ 'license': 'OPL-1',
+ 'website': '',
+ 'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'data/cron.xml',
+        'views/product_import_batch_view.xml',
+        'views/product_cierries_import_batch_view.xml',
+        'wizard/import_product_wizard_view.xml',
+        'wizard/import_cierres_product_wizard_view.xml',
+        'wizard/export_product_view.xml',
+        ],
+  "qweb": [
+       "static/src/xml/base.xml",
+        ],
+ 'installable': True,
+ 'application': True,
+ }
---
+++ import_product_inventory/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import product_import_batch
+from . import product_cierres_import_batch
