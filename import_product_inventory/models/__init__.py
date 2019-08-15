4789438a95638d719e0b11f33ccdfb3654b9e1aa
Ossi Mantylahti <37711285+ossimantylahti@users.noreply.github.com>
2019-08-15 17:50:14 -0500

    removed all extensions

--- import_product_inventory/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import pos_category
-from . import product_import_batch
-from . import product_cierres_import_batch
--- import_product_inventory/wizard/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import import_product_wizard
-from . import import_cierres_product_wizard
-from . import export_product_wizard
